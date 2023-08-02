78275eb98d6129371eba9c9efa32d7f2d78ddfe3
jessy521 <jessykumarsarkar@gmail.com>
2023-08-03 02:50:09 +0530

    db added

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -4,6 +4,8 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/hms'),
+      mongoose.connect(
+        'mongodb+srv://jessy521:OadW5zpr4n2p2LEF@cluster0.vhty7kb.mongodb.net/?retryWrites=true&w=majority',
+      ),
   },
 ];
